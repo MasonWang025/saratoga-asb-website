d55a398f1c9f11abf18083977cb6f54a4667217b
MasonWang025 <mason.wang0025@gmail.com>
2021-08-04 22:24:46 -0700

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    ,
+    "./data/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    ringWidth: false,
+    outline: false,
+  },
+  theme: {
+    extend: {
+      colors: {
+        toga: {
+          primary: {
+            main: "#C92128",
+            light: "#e8383f",
+            dark: "#a80c12",
+          },
+          secondary: {
+            main: "#0b122b",
+            bright: "#101a40",
+            gray: "#141c3b",
+            grayer: "#1a213b",
+            dark: "#050d2b",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ["disabled"],
+    },
+  },
+};
