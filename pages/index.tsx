2c1fb9255a65f811a378a8be8faab11150e95d10
MasonWang025 <mason.wang0025@gmail.com>
2021-08-07 10:11:38 -0700

    base site done

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Page from "../components/utility/Page";
+
+export default function Home() {
+  return (
+    <Page fullTitle="Saratoga ASB">
+      <h1>yes you can see me make edits</h1>
+    </Page>
+  );
+}
